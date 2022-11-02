b3216f8424a04d8dd77d1542b4710460b0ebd012
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-11-02 15:25:19 +0100

    update style.css

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const key = "7d3c336ea0f76d099742f9efdbd1654d"
 async function loadWeather(searchTerm){
     const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${key}`, {mode: 'cors'})
     const weatherData = await response.json()
+    console.log(weatherData)
     const city = weatherData.city.name // e.g. Warsaw
     const country = weatherData.city.country // e.g. PL
     const forecastDescription = weatherData.list[0].weather[0].description // e.g. overhead clouds
